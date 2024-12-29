182e8f5eef2bbcfb707d0df06b2f751a6f49c763
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-29 12:09:55 -0500

    configuracion del proyecto

---
+++ src/main/java/com/jhonatan/clase01Spring/Clase01SpringApplication.java
@@ -0,0 +1,13 @@
+package com.jhonatan.clase01Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase01SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase01SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhonatan/clase01Spring/Clase01SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatan.clase01Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase01SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
