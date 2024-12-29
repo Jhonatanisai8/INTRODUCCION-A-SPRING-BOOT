5d575c38c0547ac12b86ae1f9d4e51577d61e096
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-29 12:36:10 -0500

    implementacion de una api rest

---
+++ src/main/java/com/jhonatan/clase01Spring/controllers/ClienteController.java
@@ -0,0 +1,26 @@
+package com.jhonatan.clase01Spring.controllers;
+
+import com.jhonatan.clase01Spring.domain.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+    private final List<Cliente> listaClientes = new ArrayList<>(Arrays.asList(
+            new Cliente(123, "Gerardo Lopez", "gerardol", "contrasena123"),
+            new Cliente(456, "Alejandro Garcia", "alegarcia", "clave456"),
+            new Cliente(789, "Laura Sanchez", "lauras", "secreto12312"),
+            new Cliente(234, "Daniel Peralta", "danielp", "daniel234")
+    ));
+
+    @GetMapping("clientes")
+    public List<Cliente> getClientes() {
+        return listaClientes;
+    }
+
+
+}
