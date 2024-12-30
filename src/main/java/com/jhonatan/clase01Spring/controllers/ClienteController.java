05aba4fc698fd9b70ba90a90dd4544c39d5dab3e
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-30 10:07:52 -0500

    implementacion de un metodo que usa la anotacion PostMapping Y RequestBody

--- src/main/java/com/jhonatan/clase01Spring/controllers/ClienteController.java
+++ src/main/java/com/jhonatan/clase01Spring/controllers/ClienteController.java
@@ -1,9 +1,7 @@
 package com.jhonatan.clase01Spring.controllers;
 
 import com.jhonatan.clase01Spring.domain.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,24 +9,30 @@ import java.util.List;
 
 @RestController
 public class ClienteController {
-    private final List<Cliente> listaClientes = new ArrayList<>(Arrays.asList(
+    private List<Cliente> listaClientes = new ArrayList<>(Arrays.asList(
             new Cliente(123, "Gerardo Lopez", "gerardol", "contrasena123"),
             new Cliente(456, "Alejandro Garcia", "alegarcia", "clave456"),
             new Cliente(789, "Laura Sanchez", "lauras", "secreto12312"),
             new Cliente(234, "Daniel Peralta", "danielp", "daniel234")
     ));
 
-    @GetMapping("clientes")
+    @GetMapping("/clientes")
     public List<Cliente> getClientes() {
         return listaClientes;
     }
 
-    @GetMapping("clientes/{usuario}")
+    @GetMapping("/clientes/{usuario}")
     public Cliente getCliente(@PathVariable String usuario) {
         return listaClientes.stream().filter(
                 cliente -> cliente.getNombreUsuario().equalsIgnoreCase(usuario)
         ).findFirst().orElse(null);
     }
 
+
+    @PostMapping("/clientes")
+    public Cliente postCliente(@RequestBody Cliente cliente) { //recibe un valor de tipo json y lo transforma
+        this.listaClientes.add(cliente);
+        return cliente;
+    }
 
 }
